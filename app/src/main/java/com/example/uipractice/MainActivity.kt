fb363485df2ff3d62e33433393aaa0f70ec83609
eunjung04 <peunj04@gmail.com>
2020-04-07 21:31:06 +0900

    로그인 버튼 이벤트 처리 -onClick, onLongClick -Log, Toast

--- app/src/main/java/com/example/uipractice/MainActivity.kt
+++ app/src/main/java/com/example/uipractice/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.uipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
+// : extends 상속
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        loginBtn.setOnClickListener {
+
+            //로그인버튼이 눌리면 할 일 지정.
+
+            Log.d("로그인버튼", "버튼 클릭됨")
+
+            Toast.makeText(this, "로그인 버튼 눌림", Toast.LENGTH_SHORT).show()
+        }
+
+
+        loginBtn.setOnLongClickListener{
+
+            Log.d("로그인버튼", "버튼 길게 누름")
+            return@setOnLongClickListener true
+            //return@함수이름 =>해당함수의 결과로 설정 명시.
+            // return true : 롱클릭만 전용 처리.  false : 일반 클릭도 같이 실행.
+        }
     }
 }
